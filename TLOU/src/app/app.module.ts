c232a49ceb9394db55958db8bc4052b36ba62b3e
BlairMut <blair.mutabaruka3@gmail.com>
2021-07-05 18:03:51 +1000

    Making new branch

--- TLOU/src/app/app.module.ts
+++ TLOU/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MediaComponent } from './media/media.component';
+import { TeamComponent } from './team/team.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MediaComponent,
+    TeamComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
